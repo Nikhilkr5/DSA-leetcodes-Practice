6597b7612e74a2624fb0cdf26380e9d81d4fd4e6
Nikhil Kumar Thakur <nikhilkumar108thakur@gmail.com>
2026-09-18 10:50:02 +0530

    Time: 1 ms (83.47%), Space: 28.5 MB (38.18%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -0,0 +1,86 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        
+        int n = nums.size();
+        vector<int> ans(n);
+        stack<int> st;
+        
+        for(int i = 2 * n - 1; i >= 0; i--) {
+            
+            int index = i % n;
+            
+            while(!st.empty() && st.top() <= nums[index]) {
+                st.pop();
+            }
+            
+            if(i < n) {
+                
+                if(st.empty()) {
+                    ans[index] = -1;
+                }
+                else {
+                    ans[index] = st.top();
+                }
+            }
+            
+            st.push(nums[index]);
+        }
+        
+        return ans;
+    }
+};
+
+
+/*
+LC 503 – Next Greater Element II
+
+Approach: Monotonic Decreasing Stack + Circular Array
+Traversal: Right → Left, 2*n times
+Circular index: i % n
+Pop: st.top() <= nums[i]
+Answer: Stack top = NGE, else -1
+Time: O(n)
+Space: O(n)
+Pattern: Next Greater Element + Circular Array
+
+*/
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     vector<int> nextGreaterElements(vector<int>& nums) {
+        
+//         int n = nums.size();
+//         vector<int> ans(n, -1);
+//         stack<int> st;
+        
+//         // Traverse 2*n elements
+//         for(int i = 2 * n - 1; i >= 0; i--) {
+            
+//             int index = i % n;
+            
+//             while(!st.empty() && st.top() <= nums[index]) {
+//                 st.pop();
+//             }
+            
+//             // Only first n iterations correspond to actual indices
+//             if(i < n) {
+//                 if(!st.empty()) {
+//                     ans[index] = st.top();
+//                 }
+//             }
+            
+//             st.push(nums[index]);
+//         }
+        
+//         return ans;
+//     }
+// };
