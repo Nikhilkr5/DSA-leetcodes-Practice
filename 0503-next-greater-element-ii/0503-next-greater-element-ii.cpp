5c62099d1b96b304d047cd52f394f5eef5eff87a
Nikhil Kumar Thakur <nikhilkumar108thakur@gmail.com>
2026-09-19 20:37:04 +0530

    Time: 11 ms (15.04%), Space: 28.5 MB (62.93%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -32,6 +32,48 @@ public:
 };
 
 
+
+/*
+
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        
+        int n = nums.size();
+        vector<int> res(n);
+        stack<int> st;
+        
+        // First pass: put all elements in stack
+        for(int i = n - 1; i >= 0; i--) {
+            st.push(nums[i]);
+        }
+        
+        // Second pass: find NGE
+        for(int i = n - 1; i >= 0; i--) {
+            
+            while(!st.empty() && st.top() <= nums[i]) {
+                st.pop();
+            }
+            
+            if(st.empty()) {
+                res[i] = -1;
+            }
+            else {
+                res[i] = st.top();
+            }
+            
+            st.push(nums[i]);
+        }
+        
+        return res;
+    }
+};
+
+
+
+*/
+
+
 /*
 LC 503 – Next Greater Element II
 
